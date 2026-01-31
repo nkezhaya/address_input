f17f36a2cdb71a6622af3737c7c8791ea0c1aedb
Nick Kezhaya <20113+nkezhaya@users.noreply.github.com>
2026-01-31 11:54:28 -0600

    Benchmark load_countries/0

--- bench/main.exs
+++ bench/main.exs
@@ -3,7 +3,8 @@ Application.ensure_all_started(:address_input)
 Benchee.run(
   %{
     "countries/0" => fn -> AddressInput.countries() end,
-    "get_country/1" => fn -> AddressInput.get_country("US") end
+    "get_country/1" => fn -> AddressInput.get_country("US") end,
+    "load_countries/0" => fn -> AddressInput.Util.load_countries() end
   },
   memory_time: 2
 )
