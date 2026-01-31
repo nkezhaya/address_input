62cb5600001bc757d15c233a7e2b02ab0d8959c5
Nick Kezhaya <20113+nkezhaya@users.noreply.github.com>
2026-01-31 11:58:24 -0600

    0.3

--- README.md
+++ README.md
@@ -47,7 +47,7 @@ by adding `address_input` to your list of dependencies in `mix.exs`:
 ```elixir
 def deps do
   [
-    {:address_input, "~> 0.2"}
+    {:address_input, "~> 0.3"}
   ]
 end
 ```
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule AddressInput.MixProject do
   def project do
     [
       app: :address_input,
-      version: "0.2.0",
+      version: "0.3.0",
       elixir: "~> 1.19",
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
